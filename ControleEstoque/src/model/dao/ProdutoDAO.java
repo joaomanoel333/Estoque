bcbc5c9fc54f3a63ad9410e36547bb2aac14fa9a
joaomanoel333 <joaomanoel333@gmail.com>
2017-05-14 16:48:54 -0300

    Criação do DAO Fornecedor

--- ControleEstoque/src/model/dao/ProdutoDAO.java
+++ ControleEstoque/src/model/dao/ProdutoDAO.java
@@ -1,4 +1,3 @@
-
 package model.dao;
 
 import connection.ConnectionFactory;
@@ -7,28 +6,31 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import model.bean.Fornecedor;
 
-
 public class ProdutoDAO {
-    
-    public void create(Fornecedor f){
+
+    public void create(Fornecedor f) {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
-        
+
         try {
-            stmt = con.prepareStatement("INSERT INTO fornecedor (Nome,Responsavel,CNPJ,Telefone,TelefoneResponsavel,UsuarioCadastro,DataCadastro) VALUES(?,?,?,?,?,?,?)");
+            stmt = con.prepareStatement("INSERT INTO fornecedor (Nome,Responsavel,CNPJ,Telefone,TelefoneResponsavel,UsuarioCadastro) VALUES(?,?,?,?,?,?)");
             stmt.setString(1, f.getNome());
             stmt.setString(2, f.getResponsavel());
             stmt.setInt(3, f.getCNPJ());
             stmt.setInt(4, f.getTelefone());
             stmt.setInt(5, f.getTelefoneresponsavel());
             stmt.setInt(6, f.getUsuariocadastro());
-            stmt.setDate(7, f.getDataCadastro());
-            
-                    
-                    } catch (SQLException ex) {
-            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso"+ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
         }
     }
 }
